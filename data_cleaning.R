5a68eaa85ada1b63ee950e2b0c05f2e63c77803a
Le, Jenna <jenna.le@londonlife.com>
2017-11-22 19:26:42 -0500

    Fill in missing genders by using pronouns in description

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,8 +1,12 @@
 library(dplyr)
 library(tidyr)
+library(tidytext)
 
 calldata <- readxl::read_excel("CMHA/Copy of Research Report December 1 2016 to September 30 2017.xlsx");
 
+#------------------------------
+# CLEANUP NOTES
+#------------------------------
 #Aggregate the notes into one string so that now we have exactly one row per call.
 collapsed_notes <- calldata %>%
   mutate(`Call ID` = zoo::na.locf(`Call ID`, na.rm = FALSE)) %>%
@@ -13,22 +17,28 @@ collapsed_notes <- calldata %>%
 calldata_with_notes <- calldata %>%
   filter(!is.na(`Call ID`)) %>%
   select(-Notes) %>%
-  left_join(collapsed_notes)
+  left_join(collapsed_notes) %>%
+  mutate(Notes = ifelse(Notes == 'NA', NA , Notes))
 
-View(calldata_with_notes)
+rm(calldata, collapsed_notes)
 
+#------------------------------
+# MISSING AGE GROUPS
+#------------------------------
 age_groups <- calldata_with_notes$`Age Group` %>% 
   unique %>% 
   tbl_df %>%
   separate(value, c('StartAgeGroup', 'EndAgeGroup'),'-', FALSE) %>%
   filter(!is.na(StartAgeGroup), !is.na(EndAgeGroup)) %>%
   arrange(StartAgeGroup)
+
+# Create a final age group (MaxAge + ie 85+) for those who don't fall into the specified groups
 maxAge <- as.integer(max(age_groups$EndAgeGroup)) + 1
+
 age_groups <- age_groups %>%
   bind_rows(c(value = paste(maxAge, '+') , StartAgeGroup = maxAge, EndAgeGroup = 10000))
 
-age_groups
-
+# Given an age, return an age group based on unique values in the data
 get.age.group <- function(age, age_groups) {
   result <- age_groups %>%
     filter(as.integer(StartAgeGroup) <= as.integer(age) & as.integer(age) <= as.integer(EndAgeGroup)) %>%
@@ -38,21 +48,73 @@ get.age.group <- function(age, age_groups) {
   result
 }
 
-# 1096 entries without an age group, but with an age
+# 1096 entries without an age group, but have an age. Assign them age groups 
 collapsed_ages <- calldata_with_notes %>%
   filter(is.na(`Age Group`), !is.na(Age)) %>%
   select(`Call ID`,`Age Group`, Age) %>%
   rowwise() %>%
   mutate(`Age Group` = toString(get.age.group(toString(Age), age_groups)))
 
-View(collapsed_ages)
-
 cleaner_calldata <- calldata_with_notes %>%
   left_join(collapsed_ages, c("Call ID", "Age")) %>%
-  mutate(`Age Group` = ifelse(is.na(`Age Group.x`), `Age Group.y`, `Age Group.x`), Notes = ifelse(Notes == 'NA', NA , Notes)) %>%
+  mutate(`Age Group` = ifelse(is.na(`Age Group.x`), `Age Group.y`, `Age Group.x`)) %>%
   select(-`Age Group.x`, -`Age Group.y`)
 
 View(cleaner_calldata)
 
-filter(calldata_with_notes, is.na(`Age Group`), !is.na(Age))
+#Result shows no more data without age group but with age. 
 filter(cleaner_calldata, is.na(`Age Group`), !is.na(Age))
+
+rm(age_groups, maxAge, collapsed_ages)
+
+#------------------------------
+# MISSING GENDERS
+#------------------------------
+# 1,781 rows missing gender but have notes
+genderless_notes <- cleaner_calldata %>% 
+  filter(is.na(Gender), !is.na(Notes)) %>%
+  select(`Call ID`, Gender, Notes)
+
+# Gendered pronouns to look for
+female <- c("she", "her", "hers")
+male <- c("he", "him", "his")
+pronouns <- append(female,male)
+
+# Create a table of tokenized pronouns for each record with pronouns
+wordslist <- genderless_notes %>% 
+  unnest_tokens(word,Notes) %>% 
+  group_by(`Call ID`) %>% 
+  filter(word %in% pronouns)
+
+females_count<- wordslist %>% count(word %in% female) %>% 
+  filter(`word %in% female` == TRUE)
+males_count<- wordslist %>% count(word %in% male) %>% 
+  filter(`word %in% male` == TRUE)
+
+# There are 780 records with a missing gender, but gendered pronouns in the description
+# We will assign the Probable Gender if % of either pronouns is > 60%. Anything else is labeled NA
+pronouns_breakdown <- left_join(females_count, males_count, c("Call ID")) %>% 
+  mutate(n.x = ifelse(is.na(n.x), 0 , n.x), 
+         n.y = ifelse(is.na(n.y), 0, n.y)) %>% 
+  mutate('Female Pronouns' = n.x, 
+         'Male Pronouns' = n.y, 
+         'Total Pronouns' = n.x + n.y, 
+         "% Female" = n.x/(n.x + n.y), 
+         "% Male" = n.y/(n.x + n.y)) %>%
+  mutate('Probable Gender' = ifelse(
+   `% Female` > as.double(0.60), "Female",
+   ifelse(`% Male` > as.double(0.60), "Male", NA)
+  )) %>% 
+  select(-n.x, -n.y, -`word %in% male` -`word %in% female`)
+
+# Resulted in 750 values that were classified
+collapsed_genders <- pronouns_breakdown %>% select(`Call ID`, `Probable Gender`)
+
+cleanest_calldata <- cleaner_calldata %>%
+  left_join(collapsed_genders) %>%
+  mutate(`Gender` = ifelse(is.na(`Gender`), `Probable Gender`, `Gender`)) %>%
+  mutate(`Gender` = ifelse(`Gender` == 'F' || `Gender` == 'f', "Female", `Gender` )) %>% 
+  mutate(`Gender` = ifelse(`Gender` == 'M'|| `Gender` == 'm', "Male", `Gender` )) %>% 
+  select(-`Probable Gender`)
+
+View(cleanest_calldata)
